329fa496002cbe2f8fed86e2dbab7b4639243bb2
Zack Hsi <zhsi@lyft.com>
2015-02-22 12:15:53 -0800

    Add SwipeRefreshLayout

--- app/src/main/java/com/zackhsi/instagramclient/PhotosActivity.java
+++ app/src/main/java/com/zackhsi/instagramclient/PhotosActivity.java
@@ -1,6 +1,7 @@
 package com.zackhsi.instagramclient;
 
 import android.content.Context;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -25,24 +26,47 @@ public class PhotosActivity extends ActionBarActivity {
 
     private ArrayList<InstagramPhoto> photos;
     private InstagramPhotosAdapter aPhotos;
+    AsyncHttpClient client;
+    private SwipeRefreshLayout swipeContainer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_photos);
+        client = new AsyncHttpClient();
         photos = new ArrayList<>();
         aPhotos = new InstagramPhotosAdapter(this, photos);
         ListView lvPhotos = (ListView) findViewById(R.id.lvPhotos);
         lvPhotos.setAdapter(aPhotos);
+        swipeContainer = (SwipeRefreshLayout) findViewById(R.id.swipeContainer);
+        configureSwipeContainer();
         fetchPopularPhotos();
+    }
+
+    private void configureSwipeContainer() {
+        swipeContainer.setColorSchemeResources(
+                android.R.color.holo_blue_bright,
+                android.R.color.holo_green_light,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_red_light
+        );
+
+        swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                fetchPopularPhotos();
+                swipeContainer.setRefreshing(false);
+            }
+        });
     }
 
     private void fetchPopularPhotos() {
         String url = "https://api.instagram.com/v1/media/popular?client_id=" + CLIENT_ID;
-        AsyncHttpClient client = new AsyncHttpClient();
         client.get(url, null, new JsonHttpResponseHandler() {
             @Override
             public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                aPhotos.clear();
+
                 try {
                     JSONArray data = response.getJSONArray("data");
                     for (int i = 0; i < data.length(); i++) {
