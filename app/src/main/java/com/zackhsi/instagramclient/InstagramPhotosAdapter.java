e13605dcb5cf160343ad6d1a271623a5bb873b3f
Zack Hsi <zhsi@lyft.com>
2015-02-22 10:53:30 -0800

    Use ViewHolder pattern

--- app/src/main/java/com/zackhsi/instagramclient/InstagramPhotosAdapter.java
+++ app/src/main/java/com/zackhsi/instagramclient/InstagramPhotosAdapter.java
@@ -18,6 +18,16 @@ import java.util.List;
  * Created by zackhsi on 2/21/15.
  */
 public class InstagramPhotosAdapter extends ArrayAdapter<InstagramPhoto> {
+
+    private static class ViewHolder {
+        ImageView avatar;
+        TextView username;
+        TextView caption;
+        ImageView photo;
+        TextView likes;
+        TextView createdAgo;
+    }
+
     public InstagramPhotosAdapter(Context context, List<InstagramPhoto> objects) {
         super(context, android.R.layout.simple_list_item_1, objects);
     }
@@ -25,26 +35,30 @@ public class InstagramPhotosAdapter extends ArrayAdapter<InstagramPhoto> {
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         InstagramPhoto photo = getItem(position);
+        ViewHolder viewHolder;
 
         if (convertView == null) {
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_photo, parent, false);
+            viewHolder = new ViewHolder();
+            viewHolder.avatar = (ImageView) convertView.findViewById(R.id.ivAvatar);
+            viewHolder.username = (TextView) convertView.findViewById(R.id.tvUsername);
+            viewHolder.caption = (TextView) convertView.findViewById(R.id.tvCaption);
+            viewHolder.photo = (ImageView) convertView.findViewById(R.id.ivPhoto);
+            viewHolder.likes = (TextView) convertView.findViewById(R.id.tvLikes);
+            viewHolder.createdAgo = (TextView) convertView.findViewById(R.id.tvCreatedAgo);
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
         }
 
-        ImageView ivAvatar = (ImageView) convertView.findViewById(R.id.ivAvatar);
-        TextView tvUsername = (TextView) convertView.findViewById(R.id.tvUsername);
-        TextView tvCaption = (TextView) convertView.findViewById(R.id.tvCaption);
-        ImageView ivPhoto = (ImageView) convertView.findViewById(R.id.ivPhoto);
-        TextView tvLikes = (TextView) convertView.findViewById(R.id.tvLikes);
-        TextView tvCreatedAgo = (TextView) convertView.findViewById(R.id.tvCreatedAgo);
-
-        ivAvatar.setImageResource(0);
-        Picasso.with(getContext()).load(photo.userImageUrl).resize(60, 60).into(ivAvatar);
-        tvUsername.setText(photo.username);
-        tvCaption.setText(Html.fromHtml("<font color=#3D719D>" + photo.username + "</font> " + photo.caption));
-        ivPhoto.setImageResource(0);
-        Picasso.with(getContext()).load(photo.imageUrl).into(ivPhoto);
-        tvLikes.setText(photo.likesCount + " likes");
-        tvCreatedAgo.setText(getCreatedAgo(photo.createdAt));
+        viewHolder.avatar.setImageResource(0);
+        Picasso.with(getContext()).load(photo.userImageUrl).resize(60, 60).into(viewHolder.avatar);
+        viewHolder.username.setText(photo.username);
+        viewHolder.caption.setText(Html.fromHtml("<font color=#3D719D>" + photo.username + "</font> " + photo.caption));
+        viewHolder.photo.setImageResource(0);
+        Picasso.with(getContext()).load(photo.imageUrl).into(viewHolder.photo);
+        viewHolder.likes.setText(photo.likesCount + " likes");
+        viewHolder.createdAgo.setText(getCreatedAgo(photo.createdAt));
 
         return convertView;
     }
