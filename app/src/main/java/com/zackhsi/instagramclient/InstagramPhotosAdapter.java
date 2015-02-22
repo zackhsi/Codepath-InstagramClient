da755614bbfb19d962842047758ba6c6ec5587db
Zack Hsi <zhsi@lyft.com>
2015-02-21 18:03:05 -0800

    Create custom layout for photos Implement getView for custom layout

--- app/src/main/java/com/zackhsi/instagramclient/InstagramPhotosAdapter.java
+++ app/src/main/java/com/zackhsi/instagramclient/InstagramPhotosAdapter.java
@@ -1,7 +1,14 @@
 package com.zackhsi.instagramclient;
 
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
@@ -11,5 +18,23 @@ import java.util.List;
 public class InstagramPhotosAdapter extends ArrayAdapter<InstagramPhoto> {
     public InstagramPhotosAdapter(Context context, List<InstagramPhoto> objects) {
         super(context, android.R.layout.simple_list_item_1, objects);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        InstagramPhoto photo = getItem(position);
+
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_photo, parent, false);
+        }
+
+        TextView tvCaption = (TextView) convertView.findViewById(R.id.tvCaption);
+        ImageView ivPhoto = (ImageView) convertView.findViewById(R.id.ivPhoto);
+
+        tvCaption.setText(photo.caption);
+        ivPhoto.setImageResource(0);
+        Picasso.with(getContext()).load(photo.imageUrl).into(ivPhoto);
+
+        return convertView;
     }
 }
